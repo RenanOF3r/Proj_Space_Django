2ed4897e8a366b65b2ec899a1192e9b75a159770
renanofernandes <141298033+RenanOF3r@users.noreply.github.com>
2025-12-10 01:10:11 -0300

    Adjust ALLOWED_HOSTS for development and production
    Refine ALLOWED_HOSTS to include localhost, 127.0.0.1, and .vercel.app for production.

--- setup/settings.py
+++ setup/settings.py
@@ -49,7 +49,11 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"] if DEBUG else []
+ALLOWED_HOSTS = ["*"] if DEBUG else [
+    'localhost',
+    '127.0.0.1',
+    '.vercel.app',
+]
 
 
 # Application definition
